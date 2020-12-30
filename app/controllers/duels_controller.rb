a5ad822e2a19fa5a71fb68c976fefe4f4cd850d5
Doode1524 <Doode1524@gmail.com>
2020-12-30 01:51:45 -0500

    add winner to duel route

--- app/controllers/duels_controller.rb
+++ app/controllers/duels_controller.rb
@@ -13,12 +13,29 @@ class DuelsController < ApplicationController
         erb :'/duels/new'
     end
       
-    post '/duel' do
-        redirect to '/duel'
+    post '/duel' do         
+        @allied_values = []         
+        @enemy_values = []         
+        @enemy_team = Team.find_by_id(params[:enemy_team])         
+        @allied_team = Team.find_by_id(params[:current_user_team])       
+            @allied_team.champions.collect do |champ|             
+                @allied_stats = champ.attributes.except("Name", "Title","id","ChampionId")         
+            end         
+            @enemy_team.champions.collect do |champ|             
+                @enemy_stats = champ.attributes.except("Name", "Title","id","ChampionId")
+            end        
+            @allied_stats.each do |k,v|            
+                @allied_values << v           
+            end                 
+            @enemy_stats.each do |k,v|             
+                @enemy_values << v           
+            end                
+            erb :'duels/duel'     
     end
 
     get '/duel' do
+ 
         erb :'duels/duel'
-      end
+    end
 
 end
