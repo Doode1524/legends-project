880298ff70cfb85ecd92faa4938741525c4d120a
Doode1524 <Doode1524@gmail.com>
2021-01-06 23:42:10 -0500

    reroute for duel bug

--- app/controllers/duels_controller.rb
+++ app/controllers/duels_controller.rb
@@ -12,14 +12,23 @@ class DuelsController < ApplicationController
         @user = current_user
         @teams = Team.all
         @team = Team.find_by_slug(params[:slug])
+        if @team == nil
+            redirect '/users'
+        end
+        
         erb :'/duels/new'
     end
-      
+    
     post '/duel' do
         @allied_values = []         
         @enemy_values = []         
+        
         @enemy_team = Team.find_by_id(params[:enemy_team])         
         @allied_team = Team.find_by_id(params[:current_user_team])
+        if @allied_team == nil
+            redirect '/users'
+        end
+
         if current_user.id == @enemy_team.user_id
             redirect "/users"
         else         
@@ -65,10 +74,3 @@ class DuelsController < ApplicationController
     end
     
 end
-
-# @allied_stats.each do |k,v|            
-#     @allied_values << v           
-# end                 
-# @enemy_stats.each do |k,v|             
-#     @enemy_values << v           
-# end
